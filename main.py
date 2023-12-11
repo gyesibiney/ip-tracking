909deb27d1390730e40f75fd43a92a72ccb00247
gyesibiney <gyesidavid@gmail.com>
2023-12-11 15:24:43 +0100

    code updated

--- main.py
+++ main.py
@@ -22,5 +22,5 @@ myloc=folium.Map(location=coordinate,zoom_start=14, popup='my location')
 
 # Correcting the icon_color spelling
 folium.Marker(coordinate, icon=folium.Icon(color='blue', icon_color='blue', prefix='fa', icon='male')).add_to(myloc)
-folium.Circle(location= coordinate, radius=100).add_to(myloc)
+folium.Circle(location= coordinate, radius=200).add_to(myloc)
 myloc.save('mymap.html')
